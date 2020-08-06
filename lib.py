1bb46f77ecaa3d53b54b4869e6ca07113dcbd242
Carl Walsh <darthwalsh@gmail.com>
2020-08-06 00:44:26 -0700

    expand BNF to handle most of BNF ops

--- lib.py
+++ lib.py
@@ -1,6 +1,6 @@
 from pathlib import Path
+import math
 import re
-
 
 with open((Path(__file__).parent / 'productions.bnf').resolve()) as f:
   productions = f.read().split('\n\n')
@@ -10,57 +10,139 @@ bnf = {}
 for p in filter(None, productions):
   name, text = (s.strip() for s in p.split('::='))
   bnf[name] = text
-  # without_comments = re.sub(r'/\*.*?\*/', '', text)
+
 
 class Bnf:
+  """Automatic parse rule based on bnf rule text
+
+  Represents different entries using python expressions:
+    "0"             Text character is str "0"
+    #x30            Escaped character is str "0"
+    [#x30-#x39]     Character range is exclusive range(0x30, 0x40)
+    "a" "b"         Concatenation is tuple ("concat", "a", "b")
+    "a" | "b"       Choice is frozenset({"a", "b"})
+    "a"?            Option is tuple ("repeat", 0, 1, "a")
+    "a"*            Repeat is tuple ("repeat", 0, inf, "a")
+    "a"+            Repeat is tuple ("repeat", 1, inf, "a")
+    "a" × 4         Repeat is tuple ("repeat", 4, 4, "a")
+    ns-char         Rule is tuple ("rule", "ns-char")
+    dig - "0" - "1" Difference is tuple ("diff", ("rule", "dig"), "0", "1")
+    t=a⇒"-" t=b⇒"+" Switch is tuple ("switch", "t", "a", "-", "b", "+")
+  """
+
   def __init__(self, text: str):
-    self.text = text
+    self.text = re.sub(r'/\*.*?\*/', '', text).strip()
     self.i = 0
     self.expr = self.parse()
+    if self.i < len(self.text):
+      raise ValueError('remaining', self.text[self.i:self.i + 10], 'got',
+                       self.expr)
 
   def parse(self):
+    return self.parseSwitch()
+
+  def parseSwitch(self):
+    prefix = r'(\w+)\s*=\s*(\w+)\s*⇒'
+    signal = self.try_take(prefix)
+    if not signal:
+      return self.parseOr()
+
+    var, val = re.match(prefix, signal).groups()
+    switch = ["switch", var, val, self.parseOr()]
+    while self.try_take(var):
+      self.take('=')
+      switch.append(self.take(r'\w+'))
+      self.take('⇒')
+      switch.append(self.parseOr())
+
+    return tuple(switch)
+
+  def parseOr(self):
+    items = set()
+    while True:
+      items.add(self.parseConcat())
+      if not self.try_take(r'\|'):
+        if len(items) == 1:
+          [item] = items
+          return item
+        return items
+
+  def parseConcat(self):
     items = []
     while True:
-      self.take_match(r'\s*')
-      if self.i == len(self.text):
-        return items
-      c = self.pop()
-      if c == '"':
-        c = self.pop()
-        if c == '\\':
-          c = self.pop()
-        items.append(c)
-        if self.pop() != '"':
-          raise ValueError()
-      elif c == '#' and self.pop() == 'x':
-        items.append(chr(int(self.take_match(r'[0-9A-F]{1,6}'), 16)))
-      elif c == '[' and self.pop() == '#' and self.pop() == 'x':
-        begin = chr(int(self.take_match(r'[0-9A-F]{1,6}'), 16))
-        if self.pop() != '-' or self.pop() != '#' or self.pop() != 'x':
-          raise ValueError()
-        end = chr(int(self.take_match(r'[0-9A-F]{1,6}'), 16))
-        if self.pop() != ']':
-          raise ValueError()
-        items.append((begin, end))
+      item = self.parseDiff()
+      if item:
+        items.append(item)
       else:
-        raise ValueError(c)
+        if len(items) == 1:
+          [item] = items
+          return item
+        return ('concat', *items)
 
-  def pop(self):
-    c = self.text[self.i]
-    self.i += 1
-    return c
+  def parseDiff(self):
+    subtrahend = self.parseRepeat()
+    minuends = []
+    while self.try_take('-'):
+      minuends.append(self.parseRepeat())
+    if minuends:
+      return ('diff', subtrahend, *minuends)
+    return subtrahend
 
-  def take_match(self, s):
-    m = re.match(s, self.text[self.i:])
+  def parseRepeat(self):
+    e = self.parseSingle()
+    if c := self.try_take('[+?*×]'):
+      lo, hi = 0, math.inf
+      if c == '+':
+        lo = 1
+      elif c == '?':
+        hi = 1
+      elif c == '×':
+        lo = hi = int(self.take(r'\d+'))
+      return ('repeat', lo, hi, e)
+    return e
+
+  def parseSingle(self):
+    if self.try_take('"'):
+      self.try_take(r'\\')
+      c = self.take()
+      self.take('"')
+      return c
+    elif self.try_take('#x'):
+      return chr(int(self.take(r'[0-9A-F]{1,6}'), 16))
+    elif self.try_take(r'\[#x'):
+      begin = int(self.take(r'[0-9A-F]{1,6}'), 16)
+      self.take('-#x')
+      end = int(self.take(r'[0-9A-F]{1,6}'), 16) + 1
+      self.take(r'\]')
+      return range(begin, end)
+    elif name := self.try_take(r'\w[\w(),<≤-]*(?!\s*=)'):
+      # Avoid capturing strings followed by '=' because that is switch name
+      return "rule", name
+    elif self.try_take(r'\('):
+      parens = self.parse()
+      self.take(r'\)')
+      return parens
+    else:
+      return None
+
+  def try_take(self, pattern='.'):
+    m = re.match(pattern, self.text[self.i:])
     if not m:
-      raise ValueError(s)
+      return None
     s = m.group()
     self.i += len(s)
+    self.try_take(r'\s+')
+    return s
+
+  def take(self, pattern='.'):
+    s = self.try_take(pattern)
+    if not s:
+      raise ValueError('expected', pattern, 'at', self.text[self.i:self.i + 10])
     return s
 
 
-
 class Document:
+
   def __init__(self, s: str):
     self.text = s
     self.i = 0
@@ -87,10 +169,14 @@ class Document:
 
     self.until('\n')
 
-    try: return int(line)
-    except ValueError: pass
-    try: return float(line)
-    except ValueError: pass
+    try:
+      return int(line)
+    except ValueError:
+      pass
+    try:
+      return float(line)
+    except ValueError:
+      pass
     return line
 
   def parseSeq(self):
@@ -130,6 +216,7 @@ class Document:
     s = self.text[self.i:self.text.index(c, self.i)]
     self.i += len(s) + len(c)
     return s
+
 
 def yaml(s: str):
   return Document(s).o
--- test_bnf.py
+++ test_bnf.py
@@ -7,40 +7,115 @@
 """
 
 import lib
-
+import math
+import pytest
 
 def test_char():
   g = lib.Bnf('"c"')
-  assert g.expr == ['c']
+  assert g.expr == 'c'
 
 
 def test_str():
   g = lib.Bnf('"y" "a" "m" "l"')
-  assert g.expr == ['y', 'a', 'm', 'l']
+  assert g.expr == ('concat', 'y', 'a', 'm', 'l')
 
 
 def test_quote():
   g = lib.Bnf(r'"\""')
-  assert g.expr == ['"']
+  assert g.expr == '"'
 
 
 def test_backslash():
   g = lib.Bnf(r'"\\"')
-  assert g.expr == ['\\']
+  assert g.expr == '\\'
 
 
 def test_unicode():
   g = lib.Bnf('#x9')
-  assert g.expr == ['\x09']
+  assert g.expr == '\x09'
   g = lib.Bnf('#x10FFFF')
-  assert g.expr == ['\U0010ffff']
+  assert g.expr == '\U0010ffff'
 
 
 def test_range():
   g = lib.Bnf('[#x30-#x39]')
-  assert g.expr == [('0', '9')]
+  assert g.expr == range(0x30, 0x3A)
 
 
 def test_or():
   g = lib.Bnf('"0" | "9"')
-  assert g.expr == [['0'] '9')]
+  assert g.expr == {'0', '9'}
+
+
+def test_opt():
+  g = lib.Bnf('"a"?')
+  assert g.expr == ("repeat", 0, 1, "a")
+
+
+def test_star():
+  g = lib.Bnf('"a"*')
+  assert g.expr == ("repeat", 0, math.inf, "a")
+
+
+def test_plus():
+  g = lib.Bnf('"a"+')
+  assert g.expr == ("repeat", 1, math.inf, "a")
+
+
+def test_times():
+  g = lib.Bnf('"a" × 4')
+  assert g.expr == ("repeat", 4, 4, "a")
+
+
+def test_rules():
+  g = lib.Bnf('s-indent(<n)')
+  assert g.expr == ("rule", "s-indent(<n)")
+
+
+def test_diff():
+  g = lib.Bnf('dig - #x30')
+  assert g.expr == ("diff", ("rule", "dig"), "0")
+
+
+def test_2diff():
+  g = lib.Bnf('dig - #x30 - #x31')
+  assert g.expr == ("diff", ("rule", "dig"), "0", "1")
+
+
+def test_parens():
+  g = lib.Bnf('"x" (hex × 2 ) "-"')
+  assert g.expr == ("concat", "x", ("repeat", 2, 2, ("rule", "hex")), "-")
+
+
+def test_switch():
+  g = lib.Bnf('t=a⇒"-" t=b⇒"+"')
+  assert g.expr == ("switch", "t", "a", "-", "b", "+")
+
+
+def test_empty():
+  g = lib.Bnf(' ')
+  assert g.expr == ('concat',)
+
+
+def test_comment():
+  g = lib.Bnf(' /* Empty */ ')
+  assert g.expr == ('concat',)
+
+
+def test_comments():
+  g = lib.Bnf('[#x41-#x46] /* A-F */ | [#x61-#x66] /* a-f */ ')
+  assert g.expr == {range(0x41, 0x47), range(0x61, 0x67)}
+
+
+def test_remaining():
+  with pytest.raises(ValueError) as e_info:
+    lib.Bnf('"1" ^^garbage')
+  assert 'garbage' in str(e_info.value)
+  assert 'remaining' in str(e_info.value)
+
+
+def test_bad_string():
+  with pytest.raises(ValueError) as e_info:
+    lib.Bnf('"1\' "2"')
+  assert '"2"' in str(e_info.value)
+  assert 'expected' in str(e_info.value)
